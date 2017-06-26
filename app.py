07b419702dc737dd6d04a8a84b51c23fb9dda409
Crows Of War <ofwarcrows@gmail.com>
2017-06-26 17:20:26 -0500

    Request the branches

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 
 from diff import gen_diff
-from urllib import request
+import urllib.request
+import json
 
 base_url = 'https://api.github.com/repos/CrowsOfWar/AvatarMod/'
+
+branches_json = urllib.request.urlopen(base_url + 'branches').read()
+branches = json.loads(branches_json)
+print(branches)
